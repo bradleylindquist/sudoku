6b93690a67d9a23f7b148c38cf896fc9c789806d
Bradley Lindquist <bradleylindquist@Bradleys-Mac-mini.local>
2024-01-15 19:07:31 -0500

    finished game with numbers

--- sudoku.js
+++ sudoku.js
@@ -3,33 +3,69 @@ var tileSelected = null;
 
 var error = 0;
 
+// var board = [
+//   "--EKTLU-Q",
+//   "I---U-N-T",
+//   "-U---E-L-",
+//   "-QSU----K",
+//   "--N----T-",
+//   "--UI--LSE",
+//   "T-K-E---I",
+//   "UE-SN----",
+//   "SL--KQ---",
+// ];
+
 var board = [
-  "--EKTLU-Q",
-  "I---U-N-T",
-  "-U---E-L-",
-  "-QSU----K",
-  "--N----T-",
-  "--UI--LSE",
-  "T-K-E---I",
-  "UE-SN----",
-  "SL--KQ---",
+  "--74916-5",
+  "2---6-3-9",
+  "-6---7-1-",
+  "-586----4",
+  "--3----9-",
+  "--62--187",
+  "9-4-7---2",
+  "67-83----",
+  "81--45---",
 ];
 
+// var solution = [
+//   "NSEKTLUIQ",
+//   "IKLQUSNET",
+//   "QUTNIEKLS",
+//   "EQSULTINK",
+//   "LINESKQTU",
+//   "KTUIQNLSE",
+//   "TNKLEUSQI",
+//   "UEQSNITKL",
+//   "SLITKQEUN",
+// ];
+
 var solution = [
-  "NSEKTLUIQ",
-  "IKLQUSNET",
-  "QUTNIEKLS",
-  "EQSULTINK",
-  "LINESKQTU",
-  "KTUIQNLSE",
-  "TNKLEUSQI",
-  "UEQSNITKL",
-  "SLITKQEUN",
+  "387491625",
+  "241568379",
+  "569327418",
+  "758619234",
+  "123784596",
+  "496253187",
+  "934176852",
+  "675832941",
+  "812945763",
 ];
 
 window.onload = function () {
   setGame();
 };
+
+// const numberToLetterMap = {
+//   1: "L",
+//   2: "I",
+//   3: "N",
+//   4: "K",
+//   5: "Q",
+//   6: "U",
+//   7: "E",
+//   8: "S",
+//   9: "T",
+// };
 
 function setGame() {
   // letters L-T
@@ -38,7 +74,7 @@ function setGame() {
     let number = document.createElement("div");
     number.id = i;
     number.innerText = i;
-    number.addEventListener('click', selectNumber);
+    number.addEventListener("click", selectNumber);
     number.classList.add("number");
     document.getElementById("digits").appendChild(number);
   }
@@ -48,12 +84,47 @@ function setGame() {
     for (let c = 0; c < 9; c++) {
       let tile = document.createElement("div");
       tile.id = r.toString() + "-" + c.toString();
+      if (board[r][c] != "-") {
+        tile.innerText = board[r][c];
+        tile.classList.add("tile-start");
+      }
+      if (r == 2 || r == 5) {
+        tile.classList.add("horizontal-line");
+      }
+      if (c == 2 || c == 5) {
+        tile.classList.add("vertical-line");
+      }
+      tile.addEventListener("click", selectTile);
       tile.classList.add("tile");
       document.getElementById("board").append(tile);
     }
   }
 }
 
-function selectNumber({
+function selectNumber() {
+  if (numSelected != null) {
+    numSelected.classList.remove("number-selected");
+  }
+  numSelected = this;
+  numSelected.classList.add("number-selected");
+}
 
-})
+function selectTile() {
+  if (numSelected) {
+    if (this.innerText != "") {
+      return;
+    }
+
+    // 0-0, 0-1, 0-2
+    let coords = this.id.split("-");
+    let r = parseInt(coords[0]);
+    let c = parseInt(coords[1]);
+
+    if (solution[r][c] === numSelected.id) {
+      this.innerText = numSelected.id;
+    } else {
+      errors += 1;
+      document.getElementById("errors").innerText = errors;
+    }
+  }
+}
